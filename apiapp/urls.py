f7d640c1816810305c64136278e2061074f4d4da
sreejesh <sreejeshas33@gmail.com>
2023-06-03 09:42:13 +0530

    completed

--- apiapp/urls.py
+++ apiapp/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 from .views import TaskViewSet
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 
 router=DefaultRouter()
 
 router.register('task',TaskViewSet,basename='task')
 
 urlpatterns = [
-  
+    path('v1/token/', TokenObtainPairView.as_view(), name='two_jwt'),
+    path('v1/token/refresh/', TokenRefreshView.as_view(), name='refresh_token')
 ]+router.urls
